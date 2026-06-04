62efcf36eae84a13b507fb82b8fe71f411834afe
abbas khan <your_ag5577650@gmail.com>
2026-06-04 09:14:22 -0700

    name change

--- src/components/greet-section.jsx
+++ src/components/greet-section.jsx
@@ -7,7 +7,7 @@ export default function GreetSection() {
             <p style={{marginBottom:"20px"}}>Delicious Food | Luxuray Room | Best Service</p>
             <button style={{marginInline:"30px", borderRadius:"5px", padding:"5px"}}>Explorer Menu</button>
             <button style={{marginInline:"30px", borderRadius:"5px", padding:"5px"}}>View Room</button>
-            <h1 style={{marginTop:"80px", fontSize:"40px"}}> !</h1>
+            <h1 style={{marginTop:"80px", fontSize:"40px"}}>Our service! </h1>
             <p>Our service is too handle your order efficicently. we will available for you for 24/7.</p>
             <p>We have best staf that will show their best in order to satisfy custumer.</p>
             <p>We will manage your Meal Order, Table Order, Delivery and Reservation of Room for 24/7</p>
