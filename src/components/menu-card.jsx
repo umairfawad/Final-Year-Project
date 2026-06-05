3fd43b7ed654faa2067add90957a9add82154291
abbas khan <your_ag5577650@gmail.com>
2026-06-05 04:54:17 -0700

    card menu

--- src/components/menu-card.jsx
+++ src/components/menu-card.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import menu1 from "../assets/room1.avif"
-import food4 from "../assets/4 food.jpg"
+import React from "react";
+import menu1 from "../assets/room1.avif";
+import food4 from "../assets/4 food.jpg";
 
 export default function FoodMenu() {
-    return (
-        <section className='menu'>
-            <div className="food_menu">
-                <img src={food4} alt="" />
-                <p className='text' style={{ color: "white", Size: "15px" }}>Enjoy your delicious meal and Rooms</p>
-                <button className='explorer_btn' >Food Order</button>
-            </div>
-            <div className="room_menu">
-                <img src={menu1} alt=''></img>
-                {/* <p className='text' style={{ color: "black", Size: "15px" }}>Enjoy your delicious meal and Rooms</p> */}
-                <button className='room_btn' >Room Booking</button>
-            </div>
-        </section>
-    )
+  return (
+    <section className="menu-container">
+
+      <div className="menu-card food-card">
+        <img src={food4} alt="Food" />
+
+        <div className="menu-content">
+          <h3>Delicious Food</h3>
+          <p>Enjoy tasty meals from our hotel kitchen</p>
+          <button className="explorer_btn">Food Order</button>
+        </div>
+      </div>
+
+      <div className="menu-card room-card" id="room-card">
+        <img src={menu1} alt="Room" />
+
+        <div className="menu-content">
+          <h3>Luxury Rooms</h3>
+          <p>Comfortable and premium hotel rooms</p>
+          <button className="room_btn">Room Booking</button>
+        </div>
+      </div>
+
+    </section>
+  );
 }
