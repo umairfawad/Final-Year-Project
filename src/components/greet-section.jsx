42641598d3290edba7bf743a0faa9f8d14111555
unknown <hk3060951@gmail.com>
2026-06-05 16:53:00 +0500

    huzaifa

--- src/components/greet-section.jsx
+++ src/components/greet-section.jsx
@@ -4,8 +4,8 @@ export default function GreetSection() {
         <section style={{ marginBottom: "20px", color:"white" }}>
             <h1 style={{fontSize: "56px", color:"white"}}>Welcome to our Platform</h1>
             <p style={{marginBottom:"20px"}}>Delicious Food | Luxuray Room | Best Service</p>
-            <button style={{marginInline:"30px", borderRadius:"5px", padding:"5px",background:"green",padding:"6px 9px", fontWeight:"bold",color:"white"}}>Explorer Menu</button>
-            <button style={{marginInline:"30px", borderRadius:"5px", padding:"5px",background:"green",padding:"6px 9px", fontWeight:"bold",color:"white"}}>View Room</button>
+            <button style={{marginInline:"30px", borderRadius:"5px", padding:"5px",background:"green",Padding:"6px 9px", fontWeight:"bold",color:"white"}}>Explorer Menu</button>
+            <button style={{marginInline:"30px", borderRadius:"5px", padding:"5px",background:"green",Padding:"6px 9px", fontWeight:"bold",color:"white"}}>View Room</button>
             <h1 style={{marginTop:"80px", fontSize:"40px", color:"orange"}}>Our services ! </h1>
             <p>Our service is too handle your order efficicently. we will available for you for 24/7.</p>
             <p>We have best staf that will show their best in order to satisfy custumer.</p>
